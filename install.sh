f63248ac67fbf458f0bce265a8aa436db6f77e54
Sreya14 <potlurisreya@gmail.com>
2021-02-11 16:56:35 +0530

    changed install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+
+apt-key list
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 sudo apt update
 
-sudo apt install -y mongodb-org
+sudo apt install mongodb-org
 
-sudo systemctl start mongod
+sudo systemctl start mongod.service
 
 sudo systemctl status mongod
+
+sudo systemctl enable mongod
+
+mongo --eval 'db.runCommand({ connectionStatus: 1 })'
+
+sudo systemctl start mongod
 
 sudo systemctl enable mongod
 
